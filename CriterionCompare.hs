36a9779b40a4d60ac62536e796365a90be27ae81
Ben Gamari <ben@smart-cactus.org>
2016-04-18 15:53:48 +0200

    Support for relative deltas

--- CriterionCompare.hs
+++ CriterionCompare.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 import qualified Data.Map.Strict as M
+import qualified Data.Text as T
 import Control.Applicative
 import Control.Monad (forM_)
 import Data.Csv
@@ -39,12 +41,12 @@ instance FromRecord BenchResult where
       | otherwise = empty
       where bench a b c d e f g = BenchResult a (Stats b c d e f g)
 
-readResults :: FilePath -> IO [BenchResult]
+readResults :: FilePath -> IO (M.Map BenchName Stats)
 readResults fname = do
     mxs <- parseResults <$> BSL.readFile fname
     case mxs of
       Left err -> fail err
-      Right xs -> return $ V.toList xs
+      Right xs -> return $ M.fromList $ map (\(BenchResult a b) -> (a, b)) $ V.toList xs
 
 parseResults :: BSL.ByteString -> Either String (V.Vector BenchResult)
 parseResults =
@@ -53,37 +55,66 @@ parseResults =
     . filter (not . ("Name,Mean" `BSL.isPrefixOf`))
     . BSL.lines
 
-invert :: M.Map RunName [BenchResult] -> M.Map BenchName (M.Map RunName Stats)
+invert :: (Ord a, Ord b) => M.Map a (M.Map b v) -> M.Map b (M.Map a v)
 invert runs =
-    M.unionsWith M.union [ M.singleton (benchName result) $ M.singleton run (benchStats result)
-                         | (run, results) <- M.toList runs
-                         , result <- results ]
+    M.unionsWith M.union [ M.singleton bench $ M.singleton run stats
+                         | (run, results) <- M.assocs runs
+                         , (bench, stats) <- M.assocs results ]
 
-tabulate :: M.Map BenchName (M.Map RunName Stats) -> Html ()
-tabulate results =
+toTable :: M.Map BenchName (M.Map RunName (Html ())) -> Html ()
+toTable results =
     table_ $ do
         tr_ $ do
             th_ "Benchmark"
-            forM_ (M.keys $ head $ M.elems results) $ \(RunName runName) -> do
+            forM_ (M.keys $ head $ M.elems results) $ \(RunName runName) ->
                 th_ $ toHtml runName
 
         forM_ (M.assocs results) $ \(BenchName benchName, runs) -> tr_ $ do
             td_ $ toHtml benchName
-            forM_ (M.assocs runs) $ \(RunName runName, stats) -> do
-                let mean = showGFloat (Just 2) (statsMean stats) ""
-                    std = showString " ± " $ showGFloat (Just 2) (statsStd stats) ""
-                td_ $ do
-                    toHtml mean
-                    span_ [class_ "stddev"] $ toHtml std
+            forM_ (M.assocs runs) $ \(RunName runName, content) -> td_ content
+
+tabulateAbsolute :: M.Map BenchName (M.Map RunName Stats)
+                 -> M.Map BenchName (M.Map RunName (Html ()))
+tabulateAbsolute = fmap $ fmap cell
+  where
+    cell stats =
+      let mean = showGFloat (Just 2) (statsMean stats) ""
+          std = showString " ± " $ showGFloat (Just 2) (statsStd stats) ""
+      in td_ $ do
+          toHtml mean
+          span_ [class_ "stddev"] $ toHtml std
+
+tabulateRelative :: RunName -> M.Map BenchName (M.Map RunName Stats)
+                 -> M.Map BenchName (M.Map RunName (Html ()))
+tabulateRelative refRun results =
+    M.mapWithKey (\bench -> M.mapWithKey (cell bench)) results
+  where
+    cell :: BenchName -> RunName -> Stats -> Html ()
+    cell bench run stats
+      | run == refRun
+      = showAbs stats
+      | Just refStats <- M.lookup bench results >>= M.lookup refRun
+      = let rel = (statsMean stats - statsMean refStats) / statsMean refStats
+            cls = T.pack $ "stat-"++sign++show n
+              where sign = if rel > 0 then "p" else "n"
+                    n = min 10 $ max (-10) $ round $ 10*rel :: Int
+        in span_ [class_ cls] $ toHtml $ showGFloat (Just 1) (100*rel) "%"
+      | otherwise
+      = showAbs stats
+
+    showAbs stats = toHtml $ showGFloat (Just 2) (statsMean stats) ""
 
 main :: IO ()
 main = do
     results <- sequence [ M.singleton (RunName "old") <$> readResults "old.csv"
                         , M.singleton (RunName "new") <$> readResults "new.csv"
                         ]
+    --let table = tabulateAbsolute $ invert $ M.unions results
+    let table = tabulateRelative (RunName "old") $ invert $ M.unions results 
+
     renderToFile "hi.html" $ doctypehtml_ $ do
         head_ $ do
             title_ "Criterion comparison"
             meta_ [ charset_ "UTF-8" ]
             style_ ".stddev { font-size: small; } td { padding: 0 1em; }"
-        body_ $ tabulate $ invert $ M.unions results
+        body_ $ toTable table
